49d30b5f55641163b94424bfbb779089cd196acb
Tobias Hildebrand <tobus3000@tobotec.ch>
2024-05-14 20:50:16 +0200

    add docstrings and cleanup

--- custom_components/openai_service/integrations/chat_service.py
+++ custom_components/openai_service/integrations/chat_service.py
@@ -32,74 +32,187 @@ class ChatService(ABC):
 
     @property
     def entry(self):
+        """Returns the Home Assistant `ConfigEntry` object instance.
+
+        Returns:
+            ConfigEntry: `ConfigEntry` object instance
+        """
         return self._entry
 
     @entry.setter
     def entry(self, entry_obj):
+        """Store the Home Assistant `ConfigEntry` object instance.
+
+        Args:
+            entry_obj (ConfigEntry): `ConfigEntry` object instance.
+        """
         self._entry = entry_obj
 
     @property
     def response(self) -> str:
+        """Returns the response text string that was returned from a chat completion.
+
+        Returns:
+            str: Chat completion response text.
+        """
         return self._response
 
     @response.setter
     def response(self, text: str):
+        """Set the response text that was returned by a chat completion call.
+
+        Args:
+            text (str): Chat completion response text.
+        """
         self._response = text
 
     @property
     def endpoint_type(self) -> str:
+        """Returns the endpoint type.
+        Supported values are:
+          - openai
+          - custom
+
+        Returns:
+            str: The endpoint type. Can be `custom` or `openai`.
+        """
         return self._endpoint_type
 
     @endpoint_type.setter
     def endpoint_type(self, ep_type: str):
-        self._endpoint_type = ep_type
+        """Set the endpoint type.
+        Supported values are:
+          - openai
+          - custom
+        
+        Args:
+            ep_type (str): The endpoint type. Can be `custom` or `openai`.
+        """
+        if ep_type in ['custom', 'openai']:
+            self._endpoint_type = ep_type
+        else:
+            self._endpoint_type = "custom"
 
     @property
     def model(self) -> str:
+        """Returns the model to be used by the openai API.
+        Not relevant if you run your own LLM locally in most cases.
+
+        Returns:
+            str: The model name to be used with the cloud openai endpoint.
+        """
         return self._model
 
     @model.setter
     def model(self, model_string: str):
+        """Set the model name to be used with the cloud openai endpoint.
+
+        Args:
+            model_string (str): The model name to be used with the cloud openai endpoint.
+        """
         self._model = model_string
 
     @property
     def api_key(self) -> str:
+        """Returns the API key to be used with the cloud openai endpoint.
+        Not required with a local LLM.
+
+        Returns:
+            str: The OpenAI API key.
+        """
         return self._api_key
 
     @api_key.setter
     def api_key(self, key: str):
+        """Set the cloud OpenAI API key to be used.
+
+        Args:
+            key (str): The OpenAI API key.
+        """
         self._api_key = key
 
     @property
     def base_url(self):
+        """Returns the base URL to be used for the chat completion.
+
+        Returns:
+            _type_: Base URL like: `http://127.0.0.1:1234/v1
+        """
         return self._base_url
 
     @base_url.setter
     def base_url(self, url: str):
+        """Sets the base URL to be used for the chat completion.
+
+        Args:
+            url (str): Base URL like: `http://127.0.0.1:1234/v1
+        """
         self._base_url = url
 
     @property
     def max_tokens(self) -> int:
+        """Returns the maximum amount of tokens a chat completion
+        is allowed to use.
+
+        Returns:
+            int: Maximum tokens allowed for chat completion.
+        """
         return self._max_tokens
 
     @max_tokens.setter
     def max_tokens(self, tokens: int):
+        """Sets the maximum amount of tokens a chat completion
+        is allowed to use.
+
+        Args:
+            tokens (int): Maximum tokens allowed for chat completion.
+        """
         self._max_tokens = int(tokens)
 
     @property
     def mood(self) -> str:
+        """Returns the 'mood' to be used.
+        This is basically the 'prompt' that defines how the LLM should be
+        dealing with your question/message.
+
+        Returns:
+            str: Returns the 'mood' (aka 'prompt') to be used.
+        """
         return self._mood
 
     @mood.setter
     def mood(self, mood_string: str):
+        """Sets the mood to be used.
+        This is basically the 'prompt' that defines how the LLM should be
+        dealing with your question/message.
+
+        Args:
+            mood_string (str): The 'mood' (aka 'prompt') to be used.
+        """
         self._mood = mood_string
 
     @property
     def temperature(self) -> float:
+        """Returns the temperature setting of the endpoint.
+        Lower values for temperature result in more consistent outputs (e.g. 0.2),
+        while higher values generate more diverse and creative results (e.g. 1.0).
+
+        Returns:
+            float: The temperature setting of the endpoint.
+        """
         return self._temperature
 
     @temperature.setter
     def temperature(self, temp_float: float):
+        """Sets the temperature parameter.
+        Lower values for temperature result in more consistent outputs (e.g. 0.2),
+        while higher values generate more diverse and creative results (e.g. 1.0).
+        Select a temperature value based on the desired trade-off between coherence 
+        and creativity for your specific application. The temperature can range is from 0 to 2.
+
+        Args:
+            temp_float (float): The temperature setting of the endpoint.
+        """
         self._temperature = float(temp_float)
 
     @abstractmethod
@@ -181,3 +294,24 @@ class ChatService(ABC):
         """
         seg = Segmenter(language=language, clean=False)
         return seg.segment(text)
+
+    def build_messages_payload(self, call: ServiceCall) -> list:
+        """Produces a list of dictionaries to be sent in
+        the `prompt` or `messages` property or the like.
+
+        Args:
+            call (ServiceCall): The Home Assistant service call object
+
+        Returns:
+            list: A list containing one dictionary to setup the chat completion.
+        """
+        return [
+            {
+                "role": "system",
+                "content": call.data.get(
+                    "mood",
+                    self.mood
+                ),
+            },
+            {"role": "user", "content": call.data.get("message")},
+        ]
--- custom_components/openai_service/integrations/openai_service.py
+++ custom_components/openai_service/integrations/openai_service.py
@@ -21,7 +21,50 @@ class OpenAIService(ChatService):
             entry (ConfigEntry): A Home Assistant `ConfigEntry` object.
         """
         super().__init__(entry)  # Call the __init__ method of the base class
+        self.frequency_penalty = 0
         _LOGGER.debug("OpenAIService Entry data %s", str(entry.data))
+
+    @property
+    def frequency_penalty(self) -> float:
+        """The frequency penalty is a contribution that is proportional 
+        to how often a particular token has already been sampled.
+
+        Returns:
+            float: Frequency penalty. 0.0-1.0 (max 2.0)
+        """
+        return float(self._frequency_penalty)
+
+    @frequency_penalty.setter
+    def frequency_penalty(self, penalty: float):
+        """Set the frequency penalty.
+        Reasonable range is: 0.0-1.0
+        Negative values can be used to increase the likelihood of repetition
+
+        Args:
+            penalty (float): Set the frequency penalty.
+        """
+        self._frequency_penalty = float(penalty)
+
+    @property
+    def presence_penalty(self) -> float:
+        """The presence penalty is a one-off additive contribution
+        that applies to all tokens that have been sampled at least once.
+
+        Returns:
+            float: Presence penalty.
+        """
+        return float(self._presence_penalty)
+
+    @presence_penalty.setter
+    def presence_penalty(self, penalty: float):
+        """Set the presence penalty.
+        Reasonable range is: 0.0-1.0
+        Negative values can be used to increase the likelihood of repetition
+
+        Args:
+            penalty (float): Set the presence penalty.
+        """
+        self._presence_penalty = float(penalty)
 
     async def chat_completion(self, call: ServiceCall) -> ServiceResponse:
         """Responsible for driving the chat completion with the given input parameters.
@@ -37,28 +80,31 @@ class OpenAIService(ChatService):
             client = AsyncOpenAI(api_key=self.api_key)
         elif self.endpoint_type == "custom":
             client = AsyncOpenAI(base_url=self.base_url, api_key=self.api_key)
-        messages = [
-            {
-                "role": "system",
-                "content": call.data.get(
-                    "mood",
-                    self.mood
-                ),
-            },
-            {"role": "user", "content": call.data.get("message")},
-        ]
-        _LOGGER.debug("OpenAI Service Message: %s", str(messages))
         async with client as client_instance:
             response = await client_instance.chat.completions.create(
-                model=self.model,
-                messages=messages,
-                temperature=self.temperature,
-                max_tokens=self.max_tokens,
-                top_p=1,
-                frequency_penalty=0,
-                presence_penalty=0.6
+                self.build_completion_payload(call)
             )
         self.response = response.choices[0].message.content
         service_response = self.prepare_response()
         _LOGGER.debug("OpenAI Service Response: %s", str(service_response))
         return service_response
+
+    def build_completion_payload(self, call) -> dict:
+        """Returns a dictionary with OpenAI API specific properties.
+           Used to run the chat completion.
+
+        Args:
+            call (ServiceCall): The Home Assistant service call object
+
+        Returns:
+            dict: OpenAI API specific chat completion settings.
+        """
+        return {
+            "model": self.model,
+            "messages": self.build_messages_payload(call),
+            "temperature": self.temperature,
+            "max_tokens": self.max_tokens,
+            "top_p": 1,
+            "frequency_penalty": self.frequency_penalty,
+            "presence_penalty": self.presence_penalty
+        }
